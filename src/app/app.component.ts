ac24c68b6bfc8c03e18e43d203efe6e725c33f61
Devid Duma <devidd05@gmail.com>
2024-03-05 16:33:36 +0100

    first running prototype, returning raw json data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {WeatherApiService} from "./services/weather-api.service";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit {
+  data: any;
+
+  constructor(private dataService: WeatherApiService) {}
+
+  ngOnInit() {
+    this.dataService.getWeatherLocation(44.1, 19.1, "a9f15ff70141e1cd8f5ae01238c5ad1e", "metric", "english")
+      .subscribe(response => {
+      this.data = response;
+      console.log(this.data);
+    });
+  }
 }
