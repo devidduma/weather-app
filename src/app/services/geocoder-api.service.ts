36da6c9e41c9642f3f13c18fcfc3509d52aef654
Devid Duma <devidd05@gmail.com>
2024-03-06 17:27:46 +0100

    feat: OpenWeatherMap

---
+++ src/app/services/geocoder-api.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeocoderApiService {
+  baseUrlApiPath = "http://api.openweathermap.org/geo/1.0/direct?"
+
+  constructor(private http: HttpClient) { }
+
+  getLocation(
+    q: string,
+    limit: number,
+    appid: string
+  ): Observable<any> {
+    let fullUrl: string = this.baseUrlApiPath + "q=" + q.toString() +
+      "&limit=" + limit.toString() + "&appid=" + appid;
+
+    return this.http.get<Array<Location>>(fullUrl);
+  }
+}
