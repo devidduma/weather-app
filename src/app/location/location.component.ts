a34c4adf621f4be0cd2b1ef11b5bc30c399c3dd2
Devid Duma <devidd05@gmail.com>
2024-03-08 17:47:16 +0100

    feat(components): Weather and Location

---
+++ src/app/location/location.component.ts
@@ -0,0 +1,46 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Location} from "../location";
+import {GeocoderApiService} from "../services/geocoder-api.service";
+import {DecimalPipe, NgForOf} from "@angular/common";
+import {RouterLink} from "@angular/router";
+import {Weather} from "../weather";
+
+@Component({
+  selector: 'app-location',
+  standalone: true,
+  imports: [
+    NgForOf,
+    RouterLink,
+    DecimalPipe
+  ],
+  templateUrl: './location.component.html',
+  styleUrl: './location.component.css'
+})
+export class LocationComponent {
+  locations: Array<Location> = [];
+  @Output() onLocationSelected: EventEmitter<Location> = new EventEmitter<Location>();
+  @Input() weather: Weather = {} as Weather;
+
+  constructor(private geocoder: GeocoderApiService) {}
+
+  ngOnInit() {
+    //this.searchLocation("London");
+  }
+
+  searchLocation(locationName: string) {
+    this.weather = {} as Weather;
+    console.log(this.weather);
+
+    this.geocoder.getLocation(locationName, 3, "a9f15ff70141e1cd8f5ae01238c5ad1e")
+      .subscribe(response => {
+        this.locations = response;
+        console.log(this.locations);
+      });
+  }
+
+  selectLocation(location: Location) {
+    this.onLocationSelected.emit(location);
+    console.log(location);
+    this.locations = [];
+  }
+}
