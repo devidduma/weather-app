4777c83cb566d1243e91b3d6831e2acea1c98f7c
Devid Duma <devidd05@gmail.com>
2025-02-12 18:56:25 +0100

    feat(components): Weather location

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -2,7 +2,6 @@ import {Component, Input, OnInit} from '@angular/core';
 import {WeatherApiService} from "../../services/weather-api.service";
 import {DatePipe, DecimalPipe, JsonPipe, NgForOf, NgIf} from "@angular/common";
 import {Weather} from "../../models/weather";
-import {RouterLink} from "@angular/router";
 import {LocationComponent} from "../location/location.component";
 import {Location} from "../../models/location";
 
@@ -27,6 +26,14 @@ export class WeatherComponent implements OnInit {
     this.selectedLocation = location;
     this.getWeather(this.selectedLocation.lat, this.selectedLocation.lon);
     console.log("onLocationSelected executed ", location);
+  }
+
+  loc(location: Location): string {
+    let loc: string = location.name;
+    if(location.state)
+      loc = loc.concat(", ", location.state);
+    loc = loc.concat(", ", location.country);
+    return loc;
   }
 
   constructor(private dataService: WeatherApiService) {}
