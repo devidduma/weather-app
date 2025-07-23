7090206c69fb34c24d33650f631e433782b025be
Devid Duma <devidd05@gmail.com>
2025-07-23 12:30:50 +0200

    refactor(components): Weather reactive

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {WeatherApiService} from "../../services/weather-api.service";
-import {DatePipe, DecimalPipe, JsonPipe, NgForOf, NgIf} from "@angular/common";
+import {AsyncPipe, DecimalPipe, JsonPipe, NgIf} from "@angular/common";
 import {Weather} from "../../models/weather";
 import {LocationComponent} from "../location/location.component";
 import {Location} from "../../models/location";
 import {GeocoderApiService} from "../../services/geocoder-api.service";
+import {Observable, of} from "rxjs";
 
 @Component({
   selector: 'app-weather',
@@ -13,31 +14,29 @@ import {GeocoderApiService} from "../../services/geocoder-api.service";
     JsonPipe,
     LocationComponent,
     NgIf,
-    DatePipe,
-    DecimalPipe
+    DecimalPipe,
+    AsyncPipe
   ],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.css'
 })
 export class WeatherComponent implements OnInit {
   weather: Weather = {} as Weather;
-  selectedLocation: Location = {} as Location;
+  selectedLocation: Observable<Location> = of({} as Location);
 
   constructor(private dataService: WeatherApiService, private geocoderService: GeocoderApiService) {
   }
 
   ngOnInit() {
-    this.geocoderService.getSelectedLocation().subscribe(location => {
-      this.selectedLocation = location;
-      this.getWeather(this.selectedLocation.lat, this.selectedLocation.lon);
-    });
+    this.selectedLocation = this.geocoderService.getSelectedLocation();
+    this.selectedLocation.subscribe(location => {
+      this.getWeather(location.lat, location.lon).subscribe(weather => {
+        this.weather = weather;
+      });
+    })
   }
 
-  getWeather(latitude: number, longitude: number) {
-    this.dataService.getWeatherLocation(latitude, longitude, "a9f15ff70141e1cd8f5ae01238c5ad1e", "metric", "english")
-      .subscribe(response => {
-        this.weather = response;
-        console.log(this.weather);
-      });
+  getWeather(latitude: number, longitude: number): Observable<Weather> {
+    return this.dataService.getWeatherLocation(latitude, longitude, "a9f15ff70141e1cd8f5ae01238c5ad1e", "metric", "english");
   }
 }
